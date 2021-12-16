0c670f90b56494434b1eb5129fe1fa6734911bff
Java-6666 <3485812830@qq.com>
2021-12-16 16:38:16 +0800

    项目测试

--- src/cn/com/yuhui/Test01.java
+++ src/cn/com/yuhui/Test01.java
@@ -3,6 +3,7 @@ package cn.com.yuhui;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -20,5 +21,19 @@ public class Test01 {
             sum+=i;
         }
         System.out.println(sum);
+    }
+    @Test
+    public void test03(){
+        int[] arr={12,58,52,452,125,365,58};
+        for(int i=1;i<arr.length;i++){
+            for(int j=0;j< arr.length-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
