62b8d57723791b6b5507c6ac13690252dafe4a59
Java-6666 <96218445+Java-6666@users.noreply.github.com>
2021-12-16 19:31:15 +0800

    Update Test01.java

--- src/cn/com/yuhui/Test01.java
+++ src/cn/com/yuhui/Test01.java
@@ -11,8 +11,6 @@ public class Test01 {
     @Test
     public void test01(){
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
     }
     @Test
     public void test02(){
